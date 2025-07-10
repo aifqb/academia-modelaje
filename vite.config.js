85c06681b3b4dae9b88ac387bcce22fa96fcdf42
alex francesc <76713839+aifqb@users.noreply.github.com>
2025-07-10 00:03:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/aracely-modelaje/', // reemplaza con el nombre de tu repo
+  base: '/academia-modelaje/', // nombre del repo
   plugins: [react()],
-})
+});
+
 
